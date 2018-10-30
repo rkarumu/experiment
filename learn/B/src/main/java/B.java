f24a5cef820ade22c591737d32d217ffb85e2b3f
rkarumudi <rkarumudi@192.168.0.21>
2018-10-30 12:58:27 -0400

    committing maven projects A,B,C,D with parent pom

---
+++ learn/B/src/main/java/B.java
@@ -0,0 +1,13 @@
+package com.learn.b;
+
+/**
+ * Hello B!
+ *
+ */
+public class B 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello B!" );
+    }
+}
---
+++ learn/C/src/main/java/C.java
@@ -0,0 +1,13 @@
+package com.learn.c;
+
+/**
+ * Hello C!
+ *
+ */
+public class C 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello C!" );
+    }
+}
